c4ff5806ab64d078e72c699b68b68b7361f9263e
Ricardo Antonio Flores III <142627056+anotherantonioflores@users.noreply.github.com>
2024-11-03 14:41:15 -0600

    Add files via upload

---
+++ CS_Decision_Tree.R
@@ -0,0 +1,139 @@
+library(tidyverse)
+library(here)
+library(lubridate)
+library(tree)
+library(rpart)
+library(rpart.plot)
+library(ggplotify)
+library(ggplot2)
+
+data = read.delim("dow_jones_index.txt", sep = ",")
+
+summary(data)
+colSums(is.na(data))
+table(data$stock)
+
+# Initial cleaning of the complete data set
+clean_data = data %>% 
+  mutate(stock = as.factor(stock)) %>% 
+  mutate(date = mdy(date), across(c(open, high, low, close, next_weeks_open, next_weeks_close), 
+                                      ~ as.numeric(str_remove_all(.x, "\\$|,")))) %>%
+   arrange(date) %>% na.omit(clean_data) 
+
+summary(clean_data)
+colSums(is.na(clean_data))
+summary(clean_data)
+str(clean_data)
+
+# Splitting Data
+set.seed(123)
+Train_data = clean_data %>% 
+  filter(quarter == 1)
+Test_data = clean_data %>% 
+  filter(quarter ==2)
+
+# Removing unnecessary variables and creating "week" variable to use in lieu of date.
+Train_data_clean = Train_data %>% group_by(stock) %>% mutate(week = row_number()) %>%
+  ungroup() %>%
+  select(-quarter)
+
+Test_data_clean = Test_data %>% group_by(stock) %>% mutate(week = row_number()) %>%
+  ungroup() %>%
+  select(-quarter)
+
+# Plot to check volume of shares traded by date.
+ggplot(data = Train_data, aes(x =date, y = volume, group = stock, color = stock)) +
+  geom_line()
+
+# Creating initial decision tree.
+tree.dj = tree(percent_change_next_weeks_price~. -date, data = Train_data_clean)
+summary(tree.dj)
+
+levels(Train_data_clean$stock)
+
+plot(tree.dj)
+text(tree.dj)
+
+# Perform cost complexity pruning by CV
+cv.dj = cv.tree(tree.dj)
+cv.dj
+which.min(cv.dj$size)
+best_size = cv.dj$size[which.min(cv.dj$dev)]
+best_size
+
+# Plot the estimated test error rate 
+par(mfrow = c(1, 2))
+plot(cv.dj$size, cv.dj$dev, type= "b")
+plot(cv.dj$k, cv.dj$dev, type= "b")
+# Note: Best size = 5
+
+# Pruned the tree utilizing the best size (5).
+par(mfrow = c(1, 1))
+prune.dj = prune.tree(tree.dj, best = 5)
+plot(prune.dj)
+title(, main = "In-class Method")
+text(prune.dj)
+
+# Utilized alternate method to create a tree plot due to issues with stock variable
+# combining levels in tree plot.
+par(mfrow = c(1, 1))
+tree.dj2 <- rpart(percent_change_next_weeks_price~. -date, data = Train_data_clean)
+optimal_cp <- tree.dj2$cptable[which.min(tree.dj2$cptable[, "xerror"]), "CP"]
+optimal_cp
+pruned_tree <- prune(tree.dj2, cp = 0.03)
+rpart.plot(pruned_tree, main = "rpart method")
+
+# Get predictions on the test data 
+preds_dj_pruned = predict(prune.dj, newdata=Test_data_clean)
+preds_dj = predict(tree.dj, newdata=Test_data_clean)
+
+# Computed the MSE. 
+# The MSE for the pruned tree: 8.42
+# The MSE for the initial tree: 9.23
+mean((preds_dj_pruned - Test_data_clean$percent_change_next_weeks_price)^2)
+mean((preds_dj - Test_data_clean$percent_change_next_weeks_price)^2)
+
+
+
+
+
+
+################################################
+###create list of all 30 stock symbols
+stock_list = levels(Train_data$stock)
+
+#Order by stock symbol. Is this needed? I'm not sure
+Train_data_clean = Train_data_clean %>% 
+  arrange(stock)
+
+Test_data_clean = Test_data_clean %>% 
+  arrange(stock)
+
+
+#Create DF for holding MSE's for each stock
+MSE_list = as.data.frame(stock_list)
+#create empty column for MSE's
+MSE_list$MSE = 0
+i=1
+
+#Looping over each stock
+for(ticker in stock_list){
+  hold_train = Train_data_clean %>% 
+    filter(stock == ticker) %>% 
+    select(!stock)
+  
+  hold_test = Test_data_clean %>% 
+    filter(stock == ticker) %>% 
+    select(!stock)
+  
+  
+  #Put model here:
+  new_model1 = glm(percent_change_next_weeks_price ~., data = hold_train)
+  results1 = predict(new_model1, newdata = hold_test)
+  MSE_list$MSE[i] = round(mean((hold_test$percent_change_next_weeks_price - results1)^2),2)
+  i = i +1
+}
+
+head(MSE_list)
+
+
